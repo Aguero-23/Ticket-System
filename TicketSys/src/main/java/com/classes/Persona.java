421238663922b48463ed99a07131b60af65bb453
Julio <Julio@192.168.1.14>
2019-11-23 20:59:58 -0600

    First commit

---
+++ TicketSys/src/main/java/com/classes/Persona.java
@@ -0,0 +1,19 @@
+package com.classes;
+
+public class Persona {
+    private int ticket;
+
+    public Persona(int ticket) {
+        this.ticket = ticket;
+    }
+
+    public int getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(int ticket) {
+        this.ticket = ticket;
+    }
+    
+    
+}
