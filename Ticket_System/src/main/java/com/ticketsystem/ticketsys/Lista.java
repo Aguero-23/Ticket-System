8a6a7cb9ee88b16fbabedc0ea7e7ea952660af0f
Julio <Julio@192.168.1.14>
2019-11-25 18:49:17 -0600

    Funcionalidad del boton generar ticket

--- Ticket_System/src/main/java/com/ticketsystem/ticketsys/Lista.java
+++ Ticket_System/src/main/java/com/ticketsystem/ticketsys/Lista.java
@@ -7,16 +7,16 @@ public class Lista {
     public void insert(Persona p) {
         if (siguiente == null) {
             siguiente = new Nodo(p);
-        } else if (p.getIdEstruc()< siguiente.getPersona().getIdEstruc()) {
+        } else if (p.getIdEstruc() < siguiente.getPersona().getIdEstruc()) {
             Nodo aux = new Nodo(p);
             aux.setSiguiente(siguiente);
             siguiente = aux;
-        } else if (siguiente.getSiguiente()== null) {
+        } else if (siguiente.getSiguiente() == null) {
             siguiente.setSiguiente(new Nodo(p));
         } else {
             Nodo aux = siguiente;
 
-            while (aux.getSiguiente() != null && aux.getSiguiente().getPersona().getIdEstruc()< p.getIdEstruc()) {
+            while (aux.getSiguiente() != null && aux.getSiguiente().getPersona().getIdEstruc() < p.getIdEstruc()) {
                 aux = aux.getSiguiente();
             }
             Nodo temp = new Nodo(p);
@@ -24,21 +24,19 @@ public class Lista {
             aux.setSiguiente(temp);
         }
     }
-    
-    public Persona buscar(int n) {
+
+    public boolean buscar(int identificacion) {
         Nodo aux = siguiente;
         boolean encontrado = false;
-        Persona pers = null;
 
         if (siguiente != null) {
             while (aux != null && !encontrado) {
-                if (aux.getPersona().getIdEstruc()== n) {
-                    pers = aux.getPersona();
+                if (aux.getPersona().getId() == identificacion) {
                     encontrado = true;
                 }
                 aux = aux.getSiguiente();
             }
         }
-        return pers;
+        return encontrado;
     }
 }
