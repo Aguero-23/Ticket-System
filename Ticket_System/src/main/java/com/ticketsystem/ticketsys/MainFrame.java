06ec5bf7fa874e1a5747fb705a45c68888acb425
Julio <Julio@192.168.1.14>
2019-12-03 16:18:33 -0600

    fixing

--- Ticket_System/src/main/java/com/ticketsystem/ticketsys/MainFrame.java
+++ Ticket_System/src/main/java/com/ticketsystem/ticketsys/MainFrame.java
@@ -1,8 +1,8 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ * a000nd open the template in the editor.
+ */ 
 package com.ticketsystem.ticketsys;
 
 import javax.swing.JOptionPane;
@@ -15,7 +15,7 @@ public class MainFrame extends javax.swing.JFrame {
 
     /**
      * Creates new form MainFrame
-     * This is a kuik fix
+     * This is a kuik fix v2
      */
     public MainFrame() {
         initComponents();
