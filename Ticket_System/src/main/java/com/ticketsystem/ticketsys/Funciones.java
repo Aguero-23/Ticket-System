7e96717dc02b933cc4ffced50596a79f55474eed
kevinvsequeira <kevinvsequeira@instance-1.us-central1-a.c.molten-precinct-244414.internal>
2019-12-04 23:33:03 +0000

    Commentarytest

--- Ticket_System/src/main/java/com/ticketsystem/ticketsys/Funciones.java
+++ Ticket_System/src/main/java/com/ticketsystem/ticketsys/Funciones.java
@@ -3,7 +3,7 @@ package com.ticketsystem.ticketsys;
 public class Funciones {
     
     public void atiende(Nodo p){
-        System.out.println("Nombre: "+p.getPersona().getNombre()+" - Ape1: "+p.getPersona().getApellido1()+" - Ape2: "
+        System.out.println("El siguiente en la lista es Nombre: "+p.getPersona().getNombre()+" - Ape1: "+p.getPersona().getApellido1()+" - Ape2: "
                 +p.getPersona().getApellido2()+" - IdEst: "+p.getPersona().getIdEstruc()+" - Id:"+p.getPersona().getId());
     }
     
